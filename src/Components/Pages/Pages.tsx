3b308b02cfacffdc23788eaade32e2aee612a1fe
Asmaa Medhat <136195857+asmaamdht@users.noreply.github.com>
2025-10-16 16:23:33 +0300

    Update Page Component

--- src/Components/Pages/Pages.tsx
+++ src/Components/Pages/Pages.tsx
@@ -13,94 +13,43 @@ import CheckOut from "./ShippingCart/CheckOut/CheckOut";
 import React from "react";
 
 const AppRoutes: React.FC = () => {
-  const location = useLocation();
-  const withoutHeadingPages: string[] = ["/login", "/register", "/forget", "/reset"];
 
-  return (
-    <>
-      
-      {!withoutHeadingPages.includes(location.pathname) && <Heading />}
-
-      <Routes>
-        {/* Main Pages */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="home" element={<HomePage />} />
-        <Route path="menu" element={<MenuPage />} />
-        <Route path="menu/:id" element={<ProductDetailsPage />} />
-        <Route path="contactus" element={<ContactPage />} />
-
-        {/* Cart */}
-        <Route path="shippingcart" element={<ShippingCart />} />
-        <Route path="shippingcart/checkout" element={<CheckOut />} />
-
-        {/* Auth */}
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="forget" element={<ForgetPassword />} />
-        <Route path="reset" element={<ResetPassword />} />
-      </Routes>
-    </>
-  );
-};
-
-const Pages: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
-};
+    const location = useLocation();
+    const withoutHeadingPages: string[] = ["/login", "/register", "/forget", "/reset"];
 
     return (
         <>
-            {!withoutHeadingPages.includes(location.pathname) && <Heading />}
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="home" element={<HomePage />} />
-                <Route path="menu" element={<MenuPage/>} />
-                <Route path="contactus" element={<ContactPage/>} />
-                <Route path="useraccount" element={<UserAccount/>} />
+        
+        {!withoutHeadingPages.includes(location.pathname) && <Heading />}
 
+        <Routes>
+            {/* Main Pages */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="menu" element={<MenuPage />} />
+            <Route path="menu/:id" element={<ProductDetailsPage />} />
+            <Route path="contactus" element={<ContactPage />} />
 
+            {/* Cart */}
+            <Route path="shippingcart" element={<ShippingCart />} />
+            <Route path="shippingcart/checkout" element={<CheckOut />} />
 
-                {/* Cart */}
-
-                <Route path="shippingcart" element={<ShippingCart/>}/>
-                <Route path="shippingcart/checkout" element={<CheckOut/>}/>
-
-
-                {/* Auth */}
-
-                <Route path="login" element={<Login/>}   />
-                <Route path="register" element={<Register/>}   />
-                <Route path="forget" element={<ForgetPassword/>}   />
-                <Route path="reset" element={<ResetPassword/>}   />
-                
-            </Routes>
+            {/* Auth */}
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forget" element={<ForgetPassword />} />
+            <Route path="reset" element={<ResetPassword />} />
+        </Routes>
         </>
     );
-}
+};
 
-export default function Pages() {
+const Pages: React.FC = () => {
     return (
         <BrowserRouter>
-            <AppRoutes />
+        <AppRoutes />
         </BrowserRouter>
     );
-}
+};
 
-// function Pages() {
-//     return (
-//         <>
-//         <BrowserRouter>
-//                 <Heading />
-
-//         </BrowserRouter>
-//         </>
-//     );
-// }
-
-// export default Pages;
-=======
 export default Pages;
->>>>>>> 331aa459a36b0e4a6792597101b1acab07542afc
